4346ab8cc0a588e0d61ad28f7c61db8af6b2c0d6
minu <alsdn638@gmail.com>
2025-12-27 02:22:19 +0900

    remove pasting

--- index.js
+++ index.js
@@ -48,86 +48,107 @@ function gameLoop(currentTime){
 }
 requestAnimationFrame(gameLoop);
 
-function rgbDistance(c1, c2) {
-  const dr = c1[0]-c2[0];
-  const dg = c1[1]-c2[1];
-  const db = c1[2]-c2[2];
-  return Math.sqrt(dr*dr+dg*dg+db*db);
-}
-function getPixel(pixels, x, y) {
-    const i = (y * pixels.width+x) * 4;
-    return [
-        pixels.data[i],
-        pixels.data[i+1],
-        pixels.data[i+2],
-        pixels.data[i+3],
-    ];
-}
-function fileToImage(file) {
-    return new Promise((resolve) => {
-        const img = new Image();
-        img.onload = () => resolve(img);
-        img.src = URL.createObjectURL(file);
-    });
-}
-function getPixelsFromImage(img) {
-    const pasteCanvas = document.getElementById("paste");
-    const pasteCtx = canvas.getContext("2d");
+// function getPixel(pixels, x, y) {
+//     const i = (y * pixels.width+x) * 4;
+//     return [
+//         pixels.data[i],
+//         pixels.data[i+1],
+//         pixels.data[i+2],
+//         pixels.data[i+3],
+//     ];
+// }
+// function fileToImage(file) {
+//     return new Promise((resolve) => {
+//         const img = new Image();
+//         img.onload = () => resolve(img);
+//         img.src = URL.createObjectURL(file);
+//     });
+// }
+// function getPixelsFromImage(img) {
+//     const pasteCanvas = document.getElementById("paste");
+//     const pasteCtx = canvas.getContext("2d");
 
-    pasteCanvas.width = img.width;
-    pasteCanvas.height = img.height;
+//     pasteCanvas.width = img.width;
+//     pasteCanvas.height = img.height;
 
-    pasteCtx.drawImage(img, 0, 0);
-    return pasteCtx.getImageData(0, 0, img.width, img.height);
-}
+//     pasteCtx.drawImage(img, 0, 0);
+//     return pasteCtx.getImageData(0, 0, img.width, img.height);
+// }
 
-const COLOR_NAMES = ["I", "J", "L", "O", "S", "T", "Z"]
-const COLORS = [
-    [0, 255, 255, 1],
-    [0, 0, 255, 1],
-    [255, 160, 0, 1],
-    [255, 255, 0, 1],
-    [0, 255, 0, 1],
-    [255, 0, 255, 1],
-    [255, 0, 0, 1],
-]
-// const COLORS = [
-//     [49, 199, 239, 1],
-//     [90, 101, 173, 1],
-//     [239, 121, 33, 1],
-//     [247, 211, 8, 1],
-//     [66, 182, 66, 1],
-//     [173, 77, 156, 1],
-//     [239, 32, 41, 1],
-// ]
-const MIN_DIST = 130;
-function makeBoardFromPixels(pixels){
-    const h = Math.floor(game.board.h/2);
-    const pw = pixels.width/game.board.w;
-    const ph = pixels.height/h;
-    const divide = 2;
-    const divideRep = 1/divide/2;
-    for (let y=0; y<h; y++){
-        for (let x=0; x<game.board.w; x++){
-            let closest = 0;
-            let dist = MIN_DIST;
-            for (let px=0; px<divide; px++){
-                for (let py=0; py<divide; py++){
-                    const p = getPixel(pixels, Math.floor((x+px/divide+divideRep)*pw), Math.floor((y+py/divide+divideRep)*ph));
-                    for (let i=0; i<COLORS.length; i++){
-                        const d = rgbDistance(p, COLORS[i]);
-                        if (d < dist){
-                            dist = d;
-                            closest = i;
-                        }
-                    }
-                }
-            }
-            if (dist < MIN_DIST)
-                game.board.set(x, y+h, COLOR_NAMES[closest]);
-        }
-    }
-}
+// function rgba(r, g, b, a){
+//     return [r, g, b, a];
+// }
+// function getRgbDistance(color1, color2){
+//     return Math.sqrt(
+//         Math.pow(color1[0] - color2[0], 2) +
+//         Math.pow(color1[1] - color2[1], 2) +
+//         Math.pow(color1[2] - color2[2], 2)
+//     );
+// }
+// const COLOR_NAMES = ["I", "J", "L", "O", "S", "T", "Z", "G", " "];
+// const MIN_DIST = 120;
+// function makeBoardFromPixels(pixels){
+//     const h = Math.floor(game.board.h/2);
+//     const pw = pixels.width/game.board.w;
+//     const ph = pixels.height/h;
+//     const divide = 2;
+//     const divideRep = 1/divide/2;
+//     let darkestColor = rgba(0, 0, 0, 1);
+//     for (let y=0; y<h; y++){
+//         for (let x=0; x<game.board.w; x++){
+//             const p = getPixel(pixels, Math.floor((x+divideRep)*pw), Math.floor((y+divideRep)*ph));
+//             if (p[0] < darkestColor[0] || p[1] < darkestColor[1] || p[2] < darkestColor[2]){
+//                 darkestColor = p;
+//             }
+//         }
+//     }
+//     const COLORS = [
+//         rgba(58, 202, 152, 1),
+//         rgba(102, 82, 182, 1),
+//         rgba(199, 120, 69, 1),
+//         rgba(234, 221, 127, 1),
+//         rgba(151, 206, 57, 1),
+//         rgba(180, 65, 171, 1),
+//         rgba(201, 55, 65, 1),
+//         rgba(66, 66, 66, 1),
+//         darkestColor,
+//     ]; 
+//     for (let y=0; y<h; y++){
+//         for (let x=0; x<game.board.w; x++){
+//             let dist = MIN_DIST;
+//             let closest = 0;
+//             for (let px=0; px<divide; px++){
+//                 for (let py=0; py<divide; py++){
+//                     const p = getPixel(pixels, Math.floor((x+px/divide+divideRep)*pw), Math.floor((y+py/divide+divideRep)*ph));
+//                     for (let i=0; i<COLORS.length; i++){
+//                         const d = getRgbDistance(p, COLORS[i]);
+//                         if (d < dist){
+//                             dist = d;
+//                             closest = i;
+//                         }
+//                     }
+//                     if (dist < MIN_DIST){
+//                         console.log(p, dist, COLOR_NAMES[closest]);
+//                         game.board.set(x, y+h, COLOR_NAMES[closest]);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+// document.addEventListener("paste", async function (e){
+//     const items = e.clipboardData.items;
+
+//     for (const item of items){
+//         if (item.type.startsWith("image/")){
+//             const file = item.getAsFile();
+//             const img = await fileToImage(file);
+//             const pixels = getPixelsFromImage(img);
+
+//             makeBoardFromPixels(pixels);
+//         }
+//     }
+// });
 document.getElementById("edit-next").addEventListener("click", function (e){
     const nexts = prompt("enter next pieces. ex) ZLOSIJT");
     game.addQueue(nexts);
@@ -144,19 +165,6 @@ document.getElementById("edit-hold").addEventListener("click", function (e){
         game.holdType = hold;
     }
     document.activeElement.blur();
-});
-document.addEventListener("paste", async function (e){
-    const items = e.clipboardData.items;
-
-    for (const item of items){
-        if (item.type.startsWith("image/")){
-            const file = item.getAsFile();
-            const img = await fileToImage(file);
-            const pixels = getPixelsFromImage(img);
-
-            makeBoardFromPixels(pixels);
-        }
-    }
 });
 document.addEventListener("keydown", function (e){
     if (e.repeat) return;
